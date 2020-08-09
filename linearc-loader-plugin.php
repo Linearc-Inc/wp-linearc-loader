5664afcab1cfe5134b82cf8f2a9143001b41c116
Isakiye Afasha <afashaisakiye@gmail>
2020-08-09 11:14:04 +0300

    first commit

---
+++ linearc-loader-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Linearc Loader
+ * Plugin URI: http://www.linearc.biz.com/our-works/
+ * Description: This creates a nice loader for our sites.
+ * Version: 1.0
+ * Author: Isakiye Afasha
+ * Author URI: http://www.iamafasha.com
+ */
